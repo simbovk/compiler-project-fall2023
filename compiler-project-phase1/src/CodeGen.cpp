e553416b54844a75149eaa8c20030f5b54099468
Amirali Vakili <amiralivakili@Amiralis-MacBook-Air.local>
2023-12-11 16:01:53 +0330

    bug fixed

--- compiler-project-phase1/src/CodeGen.cpp
+++ compiler-project-phase1/src/CodeGen.cpp
@@ -207,8 +207,8 @@ namespace
       {
         if (*I)
         {
-
-          (Assignment *)I->accept(*this);
+          Assignment *casted_I = (Assignment *)I;
+          casted_I->accept(*this);
         }
       }
     };
@@ -268,7 +268,7 @@ namespace
       }
       if (count_bes + 1 == count_exprs && !flag_has_been_true)
       {
-        (*Node.bes_end())->accept(this *);
+        (*Node).bes_end())->accept(this *);
       }
     };
   };
