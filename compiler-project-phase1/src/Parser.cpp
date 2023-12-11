df981a114b2aa476893949837c7ff58aba9b570d
Amirali Vakili <amiralivakili@Amiralis-MacBook-Air.local>
2023-12-11 18:14:08 +0330

    bug fixed

--- compiler-project-phase1/src/Parser.cpp
+++ compiler-project-phase1/src/Parser.cpp
@@ -116,7 +116,7 @@ Expr *Parser::parseDec()
             {
                 Numbers.push_back(E);
                 countExprs++;
-                llvm::errs() << "countExprs: " << countExprs << "countIdents: " << countIdentifiers;
+                // llvm::errs() << "countExprs: " << countExprs << "countIdents: " << countIdentifiers;
             }
             else
                 goto _error;
