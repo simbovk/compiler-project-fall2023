327a26047c815879eeb3bb61611ec5e4fb722e6d
Amirali Vakili <amiralivakili@Amiralis-MacBook-Air.local>
2023-12-14 12:12:35 +0330

    bug fixed

--- compiler-project-phase1/src/CodeGen.cpp
+++ compiler-project-phase1/src/CodeGen.cpp
@@ -281,7 +281,7 @@ namespace
       for (auto I = Node.exprs_begin(), E = Node.exprs_begin(), bes_I = Node.bes_begin(), bes_E = Node.bes_end(); I != E; ++I, ++bes_I)
       {
 
-        if (I != nullptr)
+        if (*I != nullptr)
         {
           llvm::errs() << "goh2" << '\n';
           (*I)->accept(*this);
