e3253d76e41d033a2f0371cefb427664f67305c7
Amirali Vakili <amiralivakili@Amiralis-MacBook-Air.local>
2023-12-14 12:17:30 +0330

    bug fixed

--- compiler-project-phase1/src/CodeGen.cpp
+++ compiler-project-phase1/src/CodeGen.cpp
@@ -277,7 +277,7 @@ namespace
       {
         count_bes++;
       }
-      
+      llvm::errs() << Node.exprs_begin() << '\n';
       for (auto I = Node.exprs_begin(), E = Node.exprs_begin(), bes_I = Node.bes_begin(), bes_E = Node.bes_end(); I != E; ++I, ++bes_I)
       {
         llvm::errs() << "goh3" << '\n';
